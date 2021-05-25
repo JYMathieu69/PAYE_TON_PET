9cea3245fbf508473c0c7ea86069085679b67b4c
Mathieu Jolly <mathieujolly69@gmail.com>
2021-05-25 14:11:59 +0200

    create seeds part 1

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :owner, class_name: 'User'
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :customers, -> { distinct }, through: :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+Animal.destroy_all
+User.destroy_all
+
+chuck = User.new(last_name: "Norris", first_name: "Chuck", address: "Texas", telephone: "123-555-789", mail: "chucknorris@nothingelse.us")
+
+paris = User.new(last_name: "Hilton", first_name: "Paris", address: "New York City", telephone: "I havn't your time honey", mail: "beautyparis@nothingelse.nyc")
+
+jeanjacques = User.new(last_name: "Goldman", first_name: "Jean-Jacques", address: "Paris", telephone: "0601020304", mail: "jjgoldman@ilovesong.fr")
+
+seraphin = Animal.new(name: "Seraphin", species: "Pigeon", sexe: "Masculin", description: "Demandez lui ce que vous voulez, Seraphin acceptera. Faites vous plaisir, pigeonnez le...", chip: false, worms: false, vaccined: false, accessories: "Des cheveux coincés dans les pattes.", owner: chuck)
+
+luna = Animal.new(name: "Luna", species: "Chat", sexe: "Feminin", description: "Le poil doux comme la soie", chip: true, worms: true, vaccined: true, accessories: "Une ceinture Gucci", owner: jeanjacques)
+
+guillaume = Animal.new(name: "Guillaume", species: "Suricat", sexe: "Masculin", descritpion: "Le bon copain de soirée! Une grande capacité à déployer un tas de conneries :)", chip: false, worms: false, vaccined: false, accessories: "Bandana de combat", owner: paris)
+
+sophie = Animal.new(name: "Sophie", species: "Girafe", sexe: "Feminin", descritpion: "Te poussera à garder l'esprit ouvert et voir toujours plus loin.", chip: true, worms: false, vaccined: false, accessories: "Paire de lunette de soleil & un guettoblaster", owner: jeanjacques)
+
+brandon = Animal.new(name: "Brandon", species: "Etalon", race: "Pur sang arabe" sexe: "Masculin", descritpion: "Infatigable, toujours bien monté, Brandon a de beaux muscles.", chip: true, worms: true, vaccined: true, accessories: "Une gourmette avec son nom", owner: chuck)
