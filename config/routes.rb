e8689ddf42b789cdf26589c0180a247503a05f67
Patrice GERIN <patouseul@yahoo.fr>
2021-05-25 09:01:02 +0200

    routes by pat

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,10 @@
 Rails.application.routes.draw do
   root to: 'animals#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' to: 'animals#index'
+  resources :animals
+  resources :bookings, only: [:my_booking, :new, :create, :destroy]
 
-  get '/animals/new' to: 'animals#new'
-  post '/animals/new' to: 'animals/create'
+  get '/owner_bookings', to: 'bookings#my_owner_bookings'
+  get '/my_animals', to: 'animals#my_animals'    #nested dans user ??
 
-  get '/animals/:id/edit', to: 'animals#edit', as: :edit_animal
-  patch '/animals/:id', to: 'animals#update'
-
-  get '/animals/:id' to: 'animals#show' as: :animals
-  delete '/animals/:id', to: 'animals#destroy'
-
-
-  get '/bookings' to: 'bookings#my_bookings'
-  get '/bookings/new' to: 'bookings#new'
-
-  delete '/bookings' to: 'bookings#destroy'
-
-  get '/owner_bookings' to: 'bookings#my_owner_bookings'
-  get '/my_animals' to: 'animals#my_animals'    nested dans user
 end
