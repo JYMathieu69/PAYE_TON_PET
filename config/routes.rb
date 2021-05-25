7fa30178c29baa05427deed75b6a036cb5fea281
Patrice GERIN <patouseul@yahoo.fr>
2021-05-25 14:10:35 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'animals#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :animals
-  resources :bookings, only: [:my_booking, :new, :create, :destroy]
-
-  get '/owner_bookings', to: 'bookings#my_owner_bookings'
-  get '/my_animals', to: 'animals#my_animals'    #nested dans user ??
+  resources :animals, only: [:show, :index, :new, :create]  do
+    resources :bookings, only: [:new, :create]
+  end
+  get '/my_bookings', to: 'bookings#my_bookings'
+  get '/my_owner_bookings', to: 'bookings#my_owner_bookings'
+  get '/my_animals', to: 'animals#my_animals'
 
 end
