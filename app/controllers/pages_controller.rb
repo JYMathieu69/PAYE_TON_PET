7469c53544edfe3fc2f2349de45b05a892d39c37
Patrice GERIN <patouseul@yahoo.fr>
2021-05-28 15:39:31 +0200

    all titles all the times

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
     @animals = Animal.all
     @location = User.pluck(:address).uniq
     @species = Animal.pluck(:species).uniq
-    @date1 = Time.now + 1.days
     render layout: 'home'
   end
 end
